7d144e981d2cceecaa3a4ee1cf7042745d69ad5d
Silas Daniel <danielsilas23@yahoo.com>
2021-06-20 01:25:28 +0300

    Implement basic login

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,12 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
 end
