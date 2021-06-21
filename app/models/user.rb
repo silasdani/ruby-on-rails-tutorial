5bde3d2f31381fcae8a0e962526ae4e359be53ed
Silas Daniel <danielsilas23@yahoo.com>
2021-06-21 14:49:04 +0300

    Add pass reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
@@ -10,7 +10,6 @@ class User < ApplicationRecord
 
     # Returns the hash digest of the given string.
     def User.digest(string)
-
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
@@ -54,6 +53,23 @@ class User < ApplicationRecord
     # Sends activation email.
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+
+    # Sets the password reset attributes.
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # Sends password reset email.
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     private
