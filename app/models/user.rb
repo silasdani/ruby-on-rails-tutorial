80d43ded7b00a75e15bd8ec6badf72217907f290
Silas Daniel <danielsilas23@yahoo.com>
2021-06-22 00:57:25 +0300

    Add user following functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
@@ -40,10 +45,12 @@ class User < ApplicationRecord
         BCrypt::Password.new(digest).is_password?(token)
     end
 
-    # Defines a proto-feed.
-    # See "Following users" for the full implementation.
+    # Returns a user's status feed.
     def feed
-        Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships
+                    WHERE follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids})
+                    OR user_id = :user_id", user_id: id)
     end
 
     # Forgets a user.
@@ -79,6 +86,21 @@ class User < ApplicationRecord
         reset_sent_at < 2.hours.ago
     end
 
+    # Follows a user.
+    def follow(other_user)
+        following << other_user
+    end
+    
+    # Unfollows a user.
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+    
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+        following.include?(other_user)
+    end
+
     private
         # Converts email to all lower-case.
         def downcase_email
@@ -90,4 +112,5 @@ class User < ApplicationRecord
             self.activation_token = User.new_token
             self.activation_digest = User.digest(activation_token)
         end
+
 end
